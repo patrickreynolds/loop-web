2c622d70f9585b2abb1cd640e88374e74d36720c
Patrick Reynolds <reynolds@adobe.com>
2015-02-15 15:14:44 -0800

    Change :username route to send index page as well. @trivial removed commas

--- routes/index.js
+++ routes/index.js
@@ -6,73 +6,76 @@ var controller = require('../controllers'),
 
 module.exports = function(app){
 
+    // Root
     // GET /
     app.get('/',function(req, res, next) {
-        res.sendFile('index.html', {root: "public"});
+        res.sendFile('index.html', {root: "public"})
     });
 
     // Users
     // GET /users
-    app.get('/users', users.all);
+    app.get('/users', users.all)
 
     // POST /users
-    app.post('/users', users.create);
+    app.post('/users', users.create)
 
     // GET /:username
-    app.get('/:username', users.showByUsername);
+    app.get('/:username', function(req, res, next) {
+        res.sendFile('index.html', {root: "public"})
+    });
 
     // GET /:username
-    app.get('/users/:userId', users.showById);    
+    app.get('/users/:userId', users.showById)  
 
     // UPDATE /users/:id
-    app.put('/users/:userId', users.update);
+    app.put('/users/:userId', users.update)
 
     // DELETE /users/:id
-    app.delete('/users/:userId', users.destroy);
+    app.delete('/users/:userId', users.destroy)
     
 
 
     // Lists
     // GET /users/:id/lists
-    app.get('/users/:userId/lists', lists.all);
+    app.get('/users/:userId/lists', lists.all)
 
     // // POST /users/:userId/lists
-    app.post('/users/:userId/lists', lists.create);
+    app.post('/users/:userId/lists', lists.create)
 
     // // GET /users/:userId/lists/:listId
-    app.get('/users/:userId/lists/:listId', lists.show);
+    app.get('/users/:userId/lists/:listId', lists.show)
 
     // // GET /users/:userId/lists/:listId
-    app.put('/users/:userId/lists/:listId', lists.update);
+    app.put('/users/:userId/lists/:listId', lists.update)
 
     // // GET /users/:userId/lists/:listId
-    app.delete('/users/:userId/lists/:listId', lists.destroy);
+    app.delete('/users/:userId/lists/:listId', lists.destroy)
 
 
 
     // Items
     // GET /users/:userId/lists/:listId/items
-    app.get('/users/:userId/lists/:listId/items', items.all);
+    app.get('/users/:userId/lists/:listId/items', items.all)
 
     // // POST /users/:userId/lists/:listId/items
-    app.post('/users/:userId/lists/:listId/items', items.create);
+    app.post('/users/:userId/lists/:listId/items', items.create)
 
     // // GET /users/:userId/lists/:listId/items/:itemId
-    app.get('/users/:userId/lists/:listId/items/:itemId', items.show);
+    app.get('/users/:userId/lists/:listId/items/:itemId', items.show)
 
     // // GET /users/:userId/lists/:listId/items/item:id
-    app.put('/users/:userId/lists/:listId/items/:itemId', items.update);
+    app.put('/users/:userId/lists/:listId/items/:itemId', items.update)
 
     // // GET /users/:userId/lists/:listId/items/:itemId
-    app.delete('/users/:userId/lists/:listId/items/:itemId', items.destroy);
+    app.delete('/users/:userId/lists/:listId/items/:itemId', items.destroy)
 
 
 
     // Auth
     // GET /login
-    app.post('/login', auth.login);
+    app.post('/login', auth.login)
     
     // GET /signup
-    app.post('/signup', auth.signup);
+    app.post('/signup', auth.signup)
 
 };
